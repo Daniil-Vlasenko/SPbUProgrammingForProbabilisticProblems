82f35ec6a35bd2b7f5dfa99bd9de012720d9288a
Daniil Vlasenko <daniil_vlasenko01@mail.ru>
2022-12-02 20:18:51 +0300

    .

--- Task2/main.cpp
+++ Task2/main.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
 #include <fstream>
+#include <set>
 #include "/usr/include/boost/algorithm/string/split.hpp"
 #include "/usr/include/boost/algorithm/string/classification.hpp"
 
+
 int main() {
-    std::ifstream fin("../text.txt");
-    std::ofstream fout("../new_text.txt");
-    std::string tmp1, tmp2;
+    std::ifstream textFile("../text.txt");
+    std::ifstream dictionaryFile("../dictionary.txt");
+    std::ofstream new_textFile("../new_text.txt");
+    std::string strTmp1, strTmp2;
     std::vector<std::string> wordsInLine;
+    std::set<std::string> dictionary;
 
-    while(getline(fin, tmp1, '\t')) {
-        boost::split(wordsInLine, tmp1, boost::is_any_of(" ,.;:?!\"\n"), boost::token_compress_on );
+    // Читаем словарь.
+    while(getline(dictionaryFile, strTmp1, '\n')) {
+        dictionary.insert(strTmp1);
+    }
 
+    // Работаем с текстом.
+    int intTmp;
+    char charTmp;
+    while(getline(textFile, strTmp1, '\n')) {
+        // Разделяем строку по пробелам.
+        boost::split(wordsInLine, strTmp1, boost::is_any_of(" "));
+        for(std::string word: wordsInLine) {
+            // Если встречается символ, запоминаем его и удаляем из слова.
+            intTmp = word.find_first_of(".,!?:;");
+            if(intTmp != -1) {
+                charTmp = word.push_back();
+                word.pop_back();
+            }
+            // Ищем слово в словаре.
+        }
     }
 
 
