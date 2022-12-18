6fd6e69e4f0d206a118235eec01ac3af5ac0600a
Daniil Vlasenko <daniil_vlasenko01@mail.ru>
2022-12-18 09:52:24 +0300

    .

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -9,56 +9,42 @@ bool isNumber(const std::string& s) {
     return((std::istringstream(s) >> ld).eof());
 }
 
-int main() {
+int readFile(std::string fileName) {
     std::vector<std::string> columnsNames;
     std::vector<std::vector<double>> columns;
-    std::ifstream fin("../file.txt");
-    int count = 0;
+    std::ifstream fin(fileName);
+    int nColumns = 0, nRows = 0;
 
-    try {
-        if(!fin.is_open())
-            throw(1);
+    if(!fin.is_open())
+        throw std::runtime_error("Could not open file");
 
-        // Читаем названия признаков.
-        std::string tmp1, tmp2;
-        getline(fin, tmp1);
-        if(tmp1.empty())
-            throw(2);
-        std::stringstream sin1(tmp1);
-        while(getline(sin1, tmp2, '\t')) {
-            ++count;
-            columnsNames.push_back(tmp2);
-        }
-        columns.resize(count);
+    // Читаем названия признаков.
+    std::string tmp1, tmp2;
+    getline(fin, tmp1);
+    if(tmp1.empty())
+        throw std::logic_error("The file is empty.");
+    std::stringstream sin1(tmp1);
+    while(getline(sin1, tmp2, '\t')) {
+        ++nColumns;
+        columnsNames.push_back(tmp2);
+    }
+    columns.resize(nColumns);
 
-        // Читаем идивидов.
-        while(getline(fin, tmp1)) {
-            std::stringstream sin2(tmp1);
-            for (int i = 0; i < count; ++i) {
-                sin2 >> tmp2;
-                if(!isNumber(tmp2))
-                    throw 3;
-                columns[i].push_back(std::stod(tmp2));
+    // Читаем идивидов.
+    while(getline(fin, tmp1)) {
+        std::stringstream sin2(tmp1);
+        for (int i = 0; i < nColumns; ++i) {
+            sin2 >> tmp2;
+            if(!isNumber(tmp2)) {
+                throw std::logic_error("There is not a double value in column " + std::to_string(i) +
+                                        " and row " + std::to_string(nRows));
             }
+            columns[i].push_back(std::stod(tmp2));
         }
+        ++nRows;
     }
-    catch(int error) {
-        switch(error) {
-            case 1:
-                std::cout << "The file was not found.";
-                break;
-            case 2:
-                std::cout << "The file is empty.";
-                break;
-            case 3:
-                std::cout << "One of the fields is not a number.";
-                break;
-            default:
-                std::cout << "Something went wrong.";
-        }
-        return error;
-    }
-    for(int i = 0; i < count; ++i) {
+
+    for(int i = 0; i < nColumns; ++i) {
         std::cout << columnsNames[i] << " ";
         for(auto j: columns[i]) {
             std::cout << j << " ";
@@ -67,5 +53,14 @@ int main() {
     }
 
     fin.close();
+}
+
+int main() {
+    try {
+        readFile("../file.txt");
+    }
+    catch (std::exception &ex) {
+        std::cout << "Ouch! That hurts, because: " << ex.what() << "!\n";
+    }
     return 0;
 }
